1fc01ed4cc5c6d52355bf1689d05302840a4bb54
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2022-05-30 18:54:59 +0000

    setup

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # delivery-ops
+
+npm install -g generator-jhipster
+
+
+
+cd backend
+jhipster jdl ../model.jdl
+
+
+cd app
+
+npm i generator-jhipster-react-native
+
+npm i -g expo-cli
+
+jhipster --blueprints react-native jdl ../model.jdl
+
+expo upgrade 43.0.0
+
+The `offline` flag is deprecated. Remove from webpack.config.js
--- app/app/config/app-config.js
+++ app/app/config/app-config.js
@@ -5,7 +5,7 @@ const extra = Constants.manifest?.extra ?? {};
 
 export default {
   // use 10.0.2.2 for Android to connect to host machine
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'https://8080-maximiliano-deliveryops-ykb02fyi4yh.ws-us46.gitpod.io/',
   // use fixtures instead of real API requests
   useFixtures: false,
   // debug mode
